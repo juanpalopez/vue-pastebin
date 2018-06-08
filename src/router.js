78f23e9229f3abcc13fc53094ef7837be1911df5
Juan Pablo López Guzmán <juanpablo@findasense.com>
2018-06-08 17:48:52 -0500

    snippets request

--- src/router.js
+++ src/router.js
@@ -2,6 +2,7 @@ import Vue from 'vue'
 import Router from 'vue-router'
 import Home from './views/Home.vue'
 import About from './views/About.vue'
+import Snippets from './views/Snippets.vue'
 
 Vue.use(Router)
 
@@ -16,6 +17,26 @@ export default new Router({
       path: '/about',
       name: 'about',
       component: About
-    }
+    },
+    {
+      path: '/snippets',
+      name: 'snippets',
+      component: Snippets
+    },
+    {
+      path: '/snippets/create',
+      name: 'about',
+      component: About
+    },
+    {
+      path: '/snippets/:id/edit',
+      name: 'about',
+      component: About
+    },
+    {
+      path: '/snippets/:id/delete',
+      name: 'about',
+      component: About
+    },
   ]
 })
---
+++ src/views/Snippets.vue
@@ -0,0 +1,48 @@
+<template>
+  <div id="snippets">
+    <h1>Here we list the snippets</h1>
+    <table>
+      <thead>
+        <tr>
+          <td>ID</td>
+          <td>Url</td>
+          <td>Owner</td>
+          <td>Title</td>
+          <td>Code</td>
+          <td>Language</td>
+        </tr>
+      </thead>
+      <tr v-for="snippet in snippets">
+        <td>{{snippet.id}}</td>
+        <td>{{snippet.url}}</td>
+        <td>{{snippet.owner}}</td>
+        <td>{{snippet.title}}</td>
+        <td>{{snippet.code}}</td>
+        <td>{{snippet.language}}</td>
+      </tr>
+    </table>
+
+  </div>
+</template>
+
+<script>
+import axios from 'axios'
+
+export default {
+  name: "snippets",
+  mounted() {
+    //do something after mounting vue instance
+      axios.get('http://localhost:8000/snippets')
+      .then((res)=>{
+          this.snippets = res.data.results
+          console.log(this.snippets)
+      })
+  },
+  data: () => ({
+    snippets:[]
+  })
+}
+</script>
+
+<style lang="scss" scoped>
+</style>
